101818855ed0f27c925d92c0007f5eedfdd15185
Nick Bazaldua <bazalnick@gmail.com>
2020-07-31 16:46:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(string) #write a method that takes in a string
-  array = [] #empty array to put string in
-  array << string.split(" ") #insterts the split string into the array
+  array = string.split(" ") #string into array
   array.reverse
 end
 
