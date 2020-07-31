b4be07ddcdef8f23f97449430ca3cb1380bfa1c6
Nick Bazaldua <bazalnick@gmail.com>
2020-07-31 16:58:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,10 @@
 def reverse_each_word(string) #write a method that takes in a string
   array = string.split(" ") #string into array
-  new_array = [] 
+  new_array = [] #used in the following .collect block
   array.collect do |word|
     new_array << word.reverse
   end
-   return new_array.join(" ")
-  
+   return new_array.join(" ") #returning array as string
 end
 
  
